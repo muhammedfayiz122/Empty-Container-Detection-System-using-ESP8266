fd2ad53073fb21ce4b733ea6b9f210c7def71850
MUHAMMED FAYIZ M <72909537+muhammedfayiz122@users.noreply.github.com>
2024-07-20 10:03:58 +0530

    Update code_by_lezzet.ino

--- code_by_lezzet.ino
+++ code_by_lezzet.ino
@@ -1,5 +1,7 @@
 #include <EEPROM.h>
 
+//esp is working as builtin led's high and low are opposite
+
 // Pin Definitions for NodeMCU
 const int TRIG_PIN = 14;            // GPIO14   D5
 const int ECHO_PIN = 12;            // GPIO12   D6
@@ -14,9 +16,9 @@ const int NON_EMPTY_CHECK_COUNT = 7;
 // EEPROM Addresses
 const int EEPROM_MIN_DIST_ADDR = 0;
 
-long duration;
-int distance;
-int emptyCount = 0;
+long duration; // to get duration while measuring distance
+int distance; // to store distance
+int emptyCount = 0; //To track the duration for which the container remains empty.
 
 int minDist;
 int maxDist;
@@ -82,6 +84,8 @@ void setup() {
   EEPROM.begin(4);  // Initialize EEPROM
   loadSettingsFromEEPROM();
 
+  /*  If the switch is turned off while the ESP is powering up, this code snippet checks 
+  whether the switch is turned on and off twice to set up the initial distance measurement.  */
   if (digitalRead(SETTINGS_BUTTON_PIN) == HIGH) { //first switch off
     digitalWrite(LED_BUILTIN, HIGH);
     while (digitalRead(SETTINGS_BUTTON_PIN) == HIGH) {
@@ -99,7 +103,7 @@ void setup() {
           //nothing
         }
         if (digitalRead(SETTINGS_BUTTON_PIN) == LOW) { //second switch on
-          initializeSettings();
+          initializeSettings(); // set up empty distance
         }
       }
     }
@@ -115,13 +119,13 @@ void loop() {
   digitalWrite(LED_BUILTIN, LOW);
   delay(1000);
   
-  if (digitalRead(SETTINGS_BUTTON_PIN) == HIGH) {
+  if (digitalRead(SETTINGS_BUTTON_PIN) == HIGH) { // To halt esp when switch is off
     digitalWrite(LED_BUILTIN, HIGH);
     while(digitalRead(SETTINGS_BUTTON_PIN) == HIGH){}
   }
 
   distance = measureDistance();
-  while (distance == 0) { //ultrasonic sensor is not working
+  while (distance == 0) { //to halt when there is a problem with ultrasonic sensor
     if (digitalRead(SETTINGS_BUTTON_PIN) == HIGH) {
       digitalWrite(LED_BUILTIN, HIGH);
       while(digitalRead(SETTINGS_BUTTON_PIN) == HIGH){}
@@ -159,7 +163,7 @@ void loop() {
       delay(400);
     }
     int j=1;
-    while (true) {
+    while (true) { // To halt esp when switch is off
       if (digitalRead(SETTINGS_BUTTON_PIN) == HIGH) {
         digitalWrite(LED_BUILTIN, HIGH);
         while(digitalRead(SETTINGS_BUTTON_PIN) == HIGH){}
@@ -177,6 +181,7 @@ void loop() {
         break;
       }
 
+      //to control led pattern on built in led
       if (j%4 == 0) {
         digitalWrite(LED_BUILTIN, LOW);
         j = 1 ;
